7ea5795d8c1df8cf0c6b6b7df3fa900ced4f4a75
jeewon94 <jayyy1224@gmail.com>
2021-04-18 03:13:37 +0900

    04/18 (일) 모바일 랜딩페이지 시작 : 마이리스트 제작 중

---
+++ js/custom.js
@@ -0,0 +1,92 @@
+$(document).ready(function() {
+
+ // var mySwiper = new Swiper(".banner_slider", {
+ //   spaceBetween: 150,
+ //   slidesPerView: 'auto',
+ //   centeredSlides: true,
+ //   roundLengths: true,
+ //   loop: true,
+ //   navigation: {
+ //     nextEl: ".swiper-button-next",
+ //     prevEl: ".swiper-button-prev"
+ //   }
+ // });
+
+
+
+
+
+
+ 
+ var swiper = new Swiper('.banner_slider', {
+  centeredSlides: true,
+slidesPerView: 'auto',
+// lazy: {
+// 	loadPrevNext: true,
+//   },
+  spaceBetween: 10,
+  loop: true,
+// autoplay: {
+//   delay: 2000,
+// 	disableOnInteraction: false,
+// },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  // navigation: {
+  //   nextEl: '.main_wrap .btn.next',
+  //   prevEl: '.main_wrap .btn.prev',
+  // },
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+ });
+ 
+ 
+ 
+ 
+ 
+  var swiper = new Swiper(".con_slider", {
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    loop: true,
+
+  });
+ 
+
+
+  var swiper = new Swiper(".myList_slider", {
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    loop: true,
+
+  });
+ 
+
+
+
+
+
+
+
+
+
+});
